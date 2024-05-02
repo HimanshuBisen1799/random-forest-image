af2f5e95e6501f2d7bf0fc9bfe674f2f55b459f6
Himanshu Bisen <bisenji1799@gmail.com>
2024-05-02 19:42:36 +0530

    fjf

--- public/script/script.js
+++ public/script/script.js
@@ -172,3 +172,38 @@ class Slideshow {
 
 const slideshow = new Slideshow(document.querySelector('.slideshow'));
 
+// page 3 slider 
+const scroll = document.querySelector(".scroll");
+var isDown = false;
+var scrollX;
+var scrollLeft;
+
+// Mouse Up Function
+scroll.addEventListener("mouseup", () => {
+	isDown = false;
+	scroll.classList.remove("active");
+});
+
+// Mouse Leave Function
+scroll.addEventListener("mouseleave", () => {
+	isDown = false;
+	scroll.classList.remove("active");
+});
+
+// Mouse Down Function
+scroll.addEventListener("mousedown", (e) => {
+	e.preventDefault();
+	isDown = true;
+	scroll.classList.add("active");
+	scrollX = e.pageX - scroll.offsetLeft;
+	scrollLeft = scroll.scrollLeft;
+});
+
+// Mouse Move Function
+scroll.addEventListener("mousemove", (e) => {
+	if (!isDown) return;
+	e.preventDefault();
+	var element = e.pageX - scroll.offsetLeft;
+	var scrolling = (element - scrollX) * 2;
+	scroll.scrollLeft = scrollLeft - scrolling;
+});
